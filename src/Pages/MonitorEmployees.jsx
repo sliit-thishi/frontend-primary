7ddd54f0dee479bc6b9e5475e7c158e1bb134cf0
Diniki <jayakodyjadn.19@uom.lk>
2022-10-08 12:52:40 +0530

    recording btn clr logic

--- src/Pages/MonitorEmployees.jsx
+++ src/Pages/MonitorEmployees.jsx
@@ -7,24 +7,32 @@ function MonitorEmployees()
 {
     const [logicRecording, setLogicRecording] = useState(true);
     const [logicScreenshot, setLogicScreenshot] = useState(false);
+    const [logicRecordingClr, setLogicRecordingClr] = useState('#1FAFA8');
+    const [logicScreenshotClr, setLogicScreenshotClr] = useState('#8DDECB');
 
     function onclickRecording() {
         if (!logicRecording) {
             setLogicRecording(true)
+            setLogicRecordingClr('#1FAFA8')
+            setLogicScreenshotClr('#8DDECB')
             setLogicScreenshot(false)
   
         } else {
             setLogicRecording(true)
+            setLogicRecordingClr('#8DDECB')
         }
     }
 
     function onclickScreenshot() {
         if (!logicScreenshot) {
             setLogicRecording(false)
+            setLogicRecordingClr('#8DDECB')
+            setLogicScreenshotClr('#1FAFA8')
             setLogicScreenshot(true)
   
         } else {
             setLogicScreenshot(true)
+            setLogicScreenshotClr('#8DDECB')
         }
     }
 
@@ -36,20 +44,20 @@ function MonitorEmployees()
                     <tr>
                         <td>
                             <button className="hRecordingBtn"
-                            onClick={onclickRecording}>
+                            onClick={onclickRecording} style={{backgroundColor:logicRecordingClr}}>
                                 Screen Recording
                             </button>
                         </td>
                         <td>
                             <button className="hRecordingBtn"
-                            onClick={onclickScreenshot}>
+                            onClick={onclickScreenshot} style={{backgroundColor:logicScreenshotClr}}>
                                 Screenshot
                             </button>
                         </td>
                     </tr>
                 </table>
             </div>
-            {logicRecording &&  <div><ScreenRecording/></div>}
+            {logicRecording &&  <div><ScreenRecording /></div>}
             {logicScreenshot &&  <div><ScreenRecording/></div>}
         </div>
         </>
