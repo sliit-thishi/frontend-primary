ed3f260952fac081173b379e7ae8bd0df8e611cc
Diniki <jayakodyjadn.19@uom.lk>
2022-10-08 08:58:00 +0530

    design - done

--- src/Pages/AssignedEmployees.jsx
+++ src/Pages/AssignedEmployees.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import EmployeeCard from './EmployeeCard';
 import ProjectCard from './ProjectCard';
 import './Home.css';
+import user from '../Resources/user.png';
 
 function AssignedEmployees()
 {
@@ -27,14 +28,39 @@ function AssignedEmployees()
      <div>
         <div className='hLeftSubContent'>
         {!logicShowProfile&& <div className='hSearchContent'>
-           <h2 className='hSearchTitle'>Enter Employee ID</h2>
-            <input type='text'/> <button onClick={onclickSearch}>Search</button>
+           <h2 className='hOptionTitle' style={{textAlign:'left', paddingLeft:'2vw'}}>Enter Employee ID</h2>
+            <input type='text' style={{
+              height:'2.6vw',
+              width:'24vw',
+              borderRadius:'6px',
+              border:'none',
+              boxShadow:'0.2vw 0.2vw #d5dddd',
+              marginLeft:'2vw',
+            }}
+            /> <button onClick={onclickSearch}
+            style={{
+              height:'3vw',
+              width:'7vw',
+              color:'white',
+              borderRadius:'7px',
+              backgroundColor:'#066B66',
+              border:'none'
+            }}>Search</button>
             </div>}
             {logicShowProfile&&  <div>
-                <h1>Employee Name</h1>
+              <table>
+                <tr>
+                  <td>
+                    <img src={user}
+                    style={{height:'6vw', width:'6vw', marginLeft:'2vw'}}/>
+                  </td>
+                  <td><h2 className='hOptionTitle' style={{paddingLeft:'2vw'}}>175745-7382-A<br/> <span style={{fontSize:'1vw', fontWeight:'600'}}>Software Engineer</span></h2> </td>
+                  <td><h2 className='hOptionTitle' style={{fontWeight:'450', fontSize:'1.2vw', paddingLeft:'2vw'}}>Employee Name</h2></td>
+                </tr>
+              </table>
             </div>}
 
-            <h2 className='hSearchTitle'>This Employee can add for those Projects</h2>
+            <h2 className='hOptionTitle'>This Employee can add for those Projects</h2>
          
             {logicProjectCard&&  <div>
                 <ProjectCard/>
--- src/Pages/ProjectCard.jsx
+++ src/Pages/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Home.css';
 
 function ProjectCard()
 {
@@ -6,20 +7,29 @@ function ProjectCard()
         <>
         <div
         style={{
-            height:'4vw',
+            height:'4.5vw',
             width:'38vw',
-            backgroundColor:'yellow',
             borderRadius:'10px',
-            marginLeft: '5vw'
+            margin: '3vw 0vw 0vw 5vw',
+            boxShadow:'0.3vw 0.3vw #d5dddd',
         }}
         >
             <table>
                 <tr>
-                    <td>
-                       <p> Project Name </p>
+                    <td >
+                      <div style={{width:'15vw'}}> <h2 className='hOptionTitle'
+                       style={{fontWeight:'500', fontSize:'1.2vw'}}> Project Name </h2></div>
                     </td>
-                    <td style={{paddingLeft:'25vw'}}>
-                        <button type="primary">ADD</button>
+                    <td style={{paddingLeft:'16vw'}}>
+                        <button type="primary"
+                        style={{height:'2.4vw',
+                         width:'6vw', 
+                         color:'white', 
+                         backgroundColor:'#066B66',
+                         borderRadius:'7px',
+                         border:'none'
+                        }}
+                        >ADD</button>
                     </td>
                 </tr>
             </table>
